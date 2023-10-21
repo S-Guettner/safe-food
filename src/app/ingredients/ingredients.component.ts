1e2875e0ef458b9928ca0a4d0019a8c490517e2a
Sven Güttner <sven_guettner@protonmail.com>
2023-10-21 19:59:46 +0200

    getting ingredient autocomplete

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environment/environment';
+import { HttpClient } from '@angular/common/http';
+import { Ingredient } from '../types/ingredient.model';
 
 @Component({
   selector: 'app-ingredients',
   templateUrl: './ingredients.component.html',
-  styleUrls: ['./ingredients.component.css']
+  styleUrls: ['./ingredients.component.css'],
 })
+
+
 export class IngredientsComponent {
+  constructor(private http: HttpClient) {}
   inputValue: string = '';
+  ingredients: Ingredient[] = [];
 
   onInputChange() {
     console.log('inputValue has changed:', this.inputValue);
+    const apiKey = environment.apiKey;
+    this.http
+    .get<Ingredient[]>(
+        `https://api.spoonacular.com/food/ingredients/autocomplete?query=${this.inputValue}&number=5&apiKey=${apiKey}`
+      )
+      .subscribe((res) => (this.ingredients = res));
+      this.logIngredients();
+  }
+  private logIngredients() {
+    console.log(this.ingredients);
   }
 }
