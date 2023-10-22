d31c485e95646ae8f38cc0baab63b662805bae91
Sven Güttner <sven_guettner@protonmail.com>
2023-10-22 02:10:19 +0200

    added fetch for recipes

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -9,26 +9,38 @@ import { SharedService } from '../shared.service';
   templateUrl: './ingredients.component.html',
   styleUrls: ['./ingredients.component.css'],
 })
-
-
 export class IngredientsComponent {
   constructor(private http: HttpClient, private sharedService: SharedService) {}
   inputValue: string = '';
   ingredients: Ingredient[] = [];
+  apiKey = environment.apiKey;
 
   ingredientsList: string[] = this.sharedService.ingredientList;
+  recipe: any = []
 
   onInputChange() {
-    console.log('inputValue has changed:', this.inputValue);
-    const apiKey = environment.apiKey;
     this.http
-    .get<Ingredient[]>(
-        `https://api.spoonacular.com/food/ingredients/autocomplete?query=${this.inputValue}&number=5&apiKey=${apiKey}`
+      .get<Ingredient[]>(
+        `https://api.spoonacular.com/food/ingredients/autocomplete?query=${this.inputValue}&number=5&apiKey=${this.apiKey}`
       )
       .subscribe((res) => (this.ingredients = res));
-      this.logIngredients();
+
   }
-  private logIngredients() {
-    console.log(this.ingredients);
+  private logrecipe() {
+    console.log(this.recipe);
+  }
+
+  onSubmit() {
+    const ingredientsList = this.ingredientsList
+      .map((ingredient, index) => {
+        return index === 0 ? ingredient : `,+${ingredient}`;
+      })
+      .join('')
+      .replace(/\s+/g, ''); 
+      console.log(ingredientsList)
+    this.http.get(
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsList}&number=5&apiKey=${this.apiKey}`
+    ).subscribe((res) => (this.recipe = res))
+    this.logrecipe();
   }
 }
