4f1f3bb19b1168495c893c5b41d8495b939d3ad9
Sven Güttner <sven_guettner@protonmail.com>
2023-10-23 17:42:13 +0200

    updated home page to render recipe results

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -10,15 +10,21 @@ import { SharedService } from '../shared.service';
   styleUrls: ['./ingredients.component.css'],
 })
 export class IngredientsComponent {
+  /* HttpClient for http requests */
+  /* shared service for gloabal variables */
   constructor(private http: HttpClient, private sharedService: SharedService) {}
+  
   inputValue: string = '';
   ingredients: Ingredient[] = [];
   apiKey = environment.apiKey;
 
   ingredientsList: string[] = this.sharedService.ingredientList;
-  recipe: any = []
+  recipes: any = []
+
+  menuListOpen: boolean = false
 
   onInputChange() {
+    /* autocomplete ingredient search */
     this.http
       .get<Ingredient[]>(
         `https://api.spoonacular.com/food/ingredients/autocomplete?query=${this.inputValue}&number=5&apiKey=${this.apiKey}`
@@ -26,21 +32,24 @@ export class IngredientsComponent {
       .subscribe((res) => (this.ingredients = res));
 
   }
-  private logrecipe() {
-    console.log(this.recipe);
-  }
 
+  /* getting recipe from choosen ingredients */
   onSubmit() {
+    this.menuListOpen = !this.menuListOpen
+    /* creates url for recipe request */
     const ingredientsList = this.ingredientsList
       .map((ingredient, index) => {
         return index === 0 ? ingredient : `,+${ingredient}`;
       })
       .join('')
+      /* remove spaces */
       .replace(/\s+/g, ''); 
       console.log(ingredientsList)
     this.http.get(
       `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsList}&number=5&apiKey=${this.apiKey}`
-    ).subscribe((res) => (this.recipe = res))
-    this.logrecipe();
+    ).subscribe((res) => (this.recipes = res))
+    
   }
+
+  
 }
