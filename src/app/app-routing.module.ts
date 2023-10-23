45903b2ab9e6e3f75a09957418deefbfeea761fd
Sven Güttner <sven_guettner@protonmail.com>
2023-10-23 17:39:20 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { DetailsComponent } from './details/details.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IngredientsComponent },
+  { path: 'details/:id', component: DetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
